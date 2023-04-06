496a4c33643997e6b2ffbe428e166fa1147937da
Anastasia Nezgovorova <odabcrfake@gmail.com>
2023-04-06 10:57:37 +0400

    Updated radius_of_random in game_engine.py.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -5,7 +5,7 @@ import brain_games.cli
 
 
 def generate_rand_num():
-    radius_of_random = (1, 20)
+    radius_of_random = (1, 30)
     rand_num = randint(radius_of_random[0], radius_of_random[1])
     return rand_num
 
