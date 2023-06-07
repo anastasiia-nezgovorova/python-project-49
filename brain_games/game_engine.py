c64404fa9c974b73aafa5f70d27cffaa1797f286
Anastasia Nezgovorova <odabcrfake@gmail.com>
2023-06-07 19:06:45 +0400

    Refactored game_engine.py: simplified the conditions in the run_game function loop.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -14,6 +14,8 @@ def run_game(task_text, game_func):
     u_name = welcome_user()
     print(task_text)
     i = 1
+    answer = ''
+    correct_answer = ''
 
     while i <= ROUNDS_AMOUNT:
         question, correct_answer = game_func()
@@ -21,14 +23,15 @@ def run_game(task_text, game_func):
         print(question)
         answer = prompt.string('Your answer: ')
 
-        if answer == correct_answer and i < ROUNDS_AMOUNT:
-            i += 1
-            print('Correct!')
-
-        elif answer == correct_answer and i == ROUNDS_AMOUNT:
-            print(f'Congratulations, {u_name}!')
-            break
-        else:
-            i += 3
+        if answer != correct_answer:
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")  # noqa: E501
             print(f"Let's try again, {u_name}!")
+            break
+
+        if i == ROUNDS_AMOUNT:
+            print(f'Congratulations, {u_name}!')
+            break
+
+        if i < ROUNDS_AMOUNT:
+            i += 1
+            print('Correct!')
