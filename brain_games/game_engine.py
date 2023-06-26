5d3723167813b6d97984f7582f268e608db67e9a
Anastasia Nezgovorova <odabcrfake@gmail.com>
2023-06-26 22:04:08 +0400

    Renamed variables and constants.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -9,27 +9,27 @@ def welcome_user():
     return name
 
 
-def run_game(task_text, game_func):
+def run_game(task_text, get_round_data):
     print('Welcome to the Brain Games!')
-    u_name = welcome_user()
+    user_name = welcome_user()
     print(task_text)
     i = 1
     answer = ''
     correct_answer = ''
 
     while i <= ROUNDS_AMOUNT:
-        question, correct_answer = game_func()
+        question, correct_answer = get_round_data()
         question = f'Question: {question}'
         print(question)
         answer = prompt.string('Your answer: ')
 
         if answer != correct_answer:
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")  # noqa: E501
-            print(f"Let's try again, {u_name}!")
+            print(f"Let's try again, {user_name}!")
             break
 
         if i == ROUNDS_AMOUNT:
-            print(f'Congratulations, {u_name}!')
+            print(f'Congratulations, {user_name}!')
             break
 
         if i < ROUNDS_AMOUNT:
